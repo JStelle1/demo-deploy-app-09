83eb258510467aa953e5db0afd2ff5d637012904
JStelle1 <99993717+JStelle1@users.noreply.github.com>
2022-02-18 18:37:31 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const app = require("express")();
 
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-	res.send("Hellow World");
+//app.get("", (req, res) => {
+//	res.send("<center><h1>My Recipes<h1></center>")
+//});
+
+app.get('/', function(req, res) {
+	  res.sendFile(path.join(__dirname, '/index_test.html'));
 });
 
 app.listen(PORT, () => {
